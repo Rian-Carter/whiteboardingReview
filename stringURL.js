db76b46bc93b8bf6abfea937fae798cf82022b4d
Rian Carter <R.Carter86@hotmail.com>
2022-07-29 11:32:12 -0700

    reformat layout for clarity

--- stringURL.js
+++ stringURL.js
@@ -1,14 +1,12 @@
 // Question #1: Turning Strings to URLs
 
 // URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
-
 // You may not use the replace() method or regular expressions to solve this problem. Solve the problem with and without recursion.
 
 // Input: "Jasmine Ann Jones"
-
 // Output: "Jasmine%20Ann%20Jones"
 
-
+// iterative attempt
 function replaceSpace(string){
   let newString = "";
   for(let i = 0; i < string.length; i++){
@@ -17,10 +15,12 @@ function replaceSpace(string){
     } else {
       newString = newString + string[i];
     }
-    console.log("Jasmine Ann Jones" + newString)
   }
-  console.log(newString)
 };
+
+//test
+console.log(replaceSpace("Jasmine Ann Jones")); // "Jasmine%20Ann%20Jones"
+
 
 // recursion attempt
 // w3schools.com
@@ -40,4 +40,3 @@ const replaceSpaceRecurse = (string) => {
 
 // test
 console.log(replaceSpaceRecurse("Jasmine Ann Jones")); // "Jasmine%20Ann%20Jones"
-console.log(replaceSpace("Jasmine Ann Jones")); // "Jasmine%20Ann%20Jones"
